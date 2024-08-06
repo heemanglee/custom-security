3946970592f721ff20a8f2821a1d20af38c7710f
w1shope <wishhopelee@gmail.com>
2024-08-07 02:31:54 +0900

    first commit

---
+++ src/test/java/com/example/custom_security/CustomSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custom_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
